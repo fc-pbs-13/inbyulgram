6fa82bf7b1063950ff9dd25d7db4fb73f388e3b0
bunnycast <berzzubunny@gmail.com>
2020-07-12 20:46:21 +0900

    user viewset setting 1. login 2. longout

--- inbyulgram/users/views.py
+++ inbyulgram/users/views.py
@@ -1,5 +1,9 @@
-from rest_framework import viewsets
+from django.contrib.auth import authenticate, login
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import viewsets, status
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from users.models import Profile, User
 from users.serializers import UserSerializer, ProfileUpdateSerializer, ProfileCreateSerializer
@@ -9,25 +13,28 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    @action()
+    @action(detail=False, methods=['post'])
     def login(self, request):
-        """
-        - 회원 가입이 된 유저 >> 유저네임, 이메일, 패스워드
-        - 를 활용하여 해당 유저가 올바르게 로그인 되었는지 확인하고
-        - 올바르게 로그인 된 유저라면
-        token, created = Token.objects.get_or_create(user=user)
-        이 코드를 적절히 활용해서, 토큰을 되돌려 주는 로직을 작성.
-        """
-        pass
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            login(request, user)
+            return Response({'token': token.key, 'email': user.email}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'User Does Not Exist.'}, status=status.HTTP_404_NOT_FOUND)
 
+    @action(detail=False, methods=['delete'])
     def logout(self, request):
-        """
-        self.request.user 를 하면, 해당 로그인(토큰 발급) 된 유저를 장고에서 알아서 찾아준다.
-        이 user는 토큰을 가지고 있다. 토큰을 삭제하는 로직을 작성.
-        - 디버거를 적절히 잘 활용해서, 해당 토큰을 삭제해준다.
-        - 토큰이 삭제가 된 후, response로 올바르게 삭제가 되었음을 알려준다.
-        """
-        pass
+        try:
+            request.user.quth_token.delete()
+            return Response({"message": "Successfully Log Out."}, status.HTTP_200_OK)
+        except (AttributeError, ObjectDoesNotExist):
+            return Response({"message": "Not Authorized User."}, status.HTTP_400_BAD_REQUEST)
+
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
